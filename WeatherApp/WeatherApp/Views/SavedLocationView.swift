a4db1c55635757ca260a68a7386ed53be6a34fe6
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2024-12-31 23:02:01 +0530

    celcius, fahrenheit switcher added

--- WeatherApp/WeatherApp/Views/SavedLocationView.swift
+++ WeatherApp/WeatherApp/Views/SavedLocationView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import _SwiftData_SwiftUI
 import SwipeActions
+import MapKit
 
 struct SavedLocationView: View {
     @EnvironmentObject var viewModel: ViewModel
@@ -15,6 +16,7 @@ struct SavedLocationView: View {
     @Environment(\.modelContext) private var modelContext
     @Query var locationData: [LocationDataModel]
     @Environment(\.dismiss) var dismiss
+    @State private var sort: Int = 0
     
     var body: some View {
         NavigationStack {
@@ -52,7 +54,7 @@ struct SavedLocationView: View {
                     }
                     .padding(5)
                     .frame(minWidth: 0, maxWidth: .infinity)
-                    .background(Color.secondary)
+                    .background(Color("searchBg"))
                     .cornerRadius(8)
                     List{
                         if let location = viewModel.currentLocation,
@@ -67,11 +69,10 @@ struct SavedLocationView: View {
                                 .padding()
                                 .listRowBackground(Color.clear)
                         }
-                        ForEach(locationData) { location in
+                        ForEach(locationData, id: \.id) { location in
                             if let weatherData = viewModel.findMatchingWeatherData(for: location) {
                                 SwipeView {
                                     WeatherCard(location: location.name, weatherData: weatherData, currentLocation: false)
-//                                WeatherCard(currentLocation: false)
                                 } trailingActions: { _ in
                                     SwipeAction(systemImage: "trash", backgroundColor: .red) {
                                         removeFromFavorite(location)
@@ -114,6 +115,31 @@ struct SavedLocationView: View {
                }
             }
             .background(.black)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Menu {
+                        Section {
+                            Picker(selection: $viewModel.isMetric, label: Text("")) {
+                                Label("Celcius", systemImage: "degreesign.celsius")
+                                    .tag(true)
+                                Label("Fahrenheit", systemImage: "degreesign.fahrenheit")
+                                    .tag(false)
+                            }
+                        }
+
+//                        Section() {
+//                            Button(action: {}) {
+//                                Label("Units", systemImage: "chart.bar")
+//                            }
+//                        }
+                    }
+                    label: {
+                        Label("", systemImage: "ellipsis.circle")
+                    }
+                    .preferredColorScheme(.dark)
+                    .tint(.white)
+                }
+            }
         }
         .onAppear {
             viewModel.onWeatherCardTap = false
@@ -121,12 +147,23 @@ struct SavedLocationView: View {
             Task {
                 await viewModel.checkLocationAuthorization()
                 guard let location = viewModel.currentLocation else { return }
-//                await viewModel.fetchWeatherDataFromLocation(location: location)
-                await viewModel.fetchAqiData()
+                await viewModel.fetchWeatherDataFromLocation(location: location)
             }
             for location in locationData {
                 Task {
-//                    await viewModel.fetchWeatherDataFromLocation(location: location)
+                    await viewModel.fetchWeatherDataFromLocation(location: location)
+                }
+            }
+        }
+        .onChange(of: viewModel.isMetric) {
+            viewModel.weatherDataList = []
+            Task {
+                guard let location = viewModel.currentLocation else { return }
+                await viewModel.fetchWeatherDataFromLocation(location: location)
+            }
+            for location in locationData {
+                Task {
+                    await viewModel.fetchWeatherDataFromLocation(location: location)
                 }
             }
         }
@@ -138,11 +175,6 @@ struct SavedLocationView: View {
             }
         }
         
-    }
-    
-    private func deleteItem(at offsets: IndexSet) {
-        var locations = locationData
-        locations.remove(atOffsets: offsets)
     }
     
     func removeFromFavorite(_ location: LocationDataModel) {
@@ -157,11 +189,11 @@ struct SavedLocationView: View {
 }
 
 
-//#Preview {
-//    @Previewable @StateObject var viewModel: ViewModel = ViewModel()
-//    SavedLocationView()
-//        .environmentObject(viewModel)
-//}
+#Preview {
+    @Previewable @StateObject var viewModel: ViewModel = ViewModel()
+    SavedLocationView()
+        .environmentObject(viewModel)
+}
 
 struct WeatherCard: View {
     @EnvironmentObject var viewModel: ViewModel
@@ -215,7 +247,7 @@ struct WeatherCard: View {
         }
         .padding(18)
         .frame(maxWidth: .infinity)
-        .background(WeatherBackgroundUtils.gradient(for: weatherData?.current.weather.first?.main.rawValue ?? "Clear"))
+        .background(WeatherUtils.gradient(for: weatherData?.current.weather.first?.main.rawValue ?? "Clear"))
         .cornerRadius(20)
         .onTapGesture {
             if (currentLocation) {
@@ -225,6 +257,7 @@ struct WeatherCard: View {
                 viewModel.searchedLocation = location
             }
             viewModel.weatherData = weatherData
+            viewModel.cordinates = CLLocationCoordinate2D(latitude: weatherData?.lat ?? 0, longitude: weatherData?.lon ?? 0)
             withAnimation(.spring(response: 1.0, dampingFraction: 0.7, blendDuration: 0.7)) {
                 viewModel.onWeatherCardTap = true
                 viewModel.selectedTab = "weather"
@@ -298,7 +331,7 @@ struct WeatherCard: View {
 //            else {
 //                viewModel.searchedLocation = locationName
 //            }
-////              viewModel.weatherData = weatherData
+//              viewModel.weatherData = weatherData
 //            // Simulate a tap gesture to update the view model and animate transition
 //            withAnimation(.spring(response: 1.0, dampingFraction: 0.7, blendDuration: 0.7)) {
 //                viewModel.onWeatherCardTap = true
